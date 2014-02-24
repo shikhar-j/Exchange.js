5b5193437a7af7aecb14e2da04e1a82b818896c2
Shikhar Jaiswal <shikharjaiswal@Zomato-Shikhar.local>
2014-02-24 13:28:00 +0530

    Minor text corrections

--- exchange.js
+++ exchange.js
@@ -1,7 +1,7 @@
 /**
  * Exchange.js
  * 
- * Inspired Foundation's interchange.js (foundation.zurb.com), exchange.js
+ * Inspired by Foundation's interchange.js (foundation.zurb.com), exchange.js
  * allows you to dynamically load responsive content. Like interchange.js,
  * exchange.js uses media queries to load appropriate content but overcomes
  * several drawbacks, adds more flexibility, optimizes functionality and
